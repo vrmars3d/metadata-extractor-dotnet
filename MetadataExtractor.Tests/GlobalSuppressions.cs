a258bdacf28b100334128d7a85de9de809ada9ef
vrmars <dan.ony.phone@gmail.com>
2022-10-22 22:46:59 +0100

    Remove Tests.

--- MetadataExtractor.Tests/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Drew Noakes and contributors. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE0004:Remove Unnecessary Cast", Justification = "Makes code clearer", Scope = "member", Target = "~F:MetadataExtractor.Tests.DirectoryExtensionsTest._singleValues")]
-[assembly: SuppressMessage("Style", "IDE0004:Remove Unnecessary Cast", Justification = "Makes code clearer", Scope = "member", Target = "~F:MetadataExtractor.Tests.DirectoryExtensionsTest._singleZeroValues")]
-[assembly: SuppressMessage("Style", "IDE0073:The file header does not match the required text", Justification = "Other license", Scope = "namespace", Target = "~N:System.Diagnostics.CodeAnalysis")]
